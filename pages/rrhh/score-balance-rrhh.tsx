8f62b9d6e8c1be5f79c57ee4cdf524f0094c2284
frontmappriskdr <front@mappriskdr.com>
2024-10-01 17:15:21 -0400

    Update score-balance-rrhh.tsx
    cambio de %

--- pages/rrhh/score-balance-rrhh.tsx
+++ pages/rrhh/score-balance-rrhh.tsx
@@ -24,7 +24,7 @@ export default function ScoreBalanceRrhh() {
         'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
         'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       ],
-      progressValue: 85,
+      progressValue: 90,
     },
     // Agrega más departamentos aquí
   ];
