9a7d0899febf17c51931be0f619930863dadd302
frontmappriskdr <front@mappriskdr.com>
2024-10-01 17:26:56 -0400

    Estado de buscador agregado

--- pages/rrhh/score-balance-rrhh.tsx
+++ pages/rrhh/score-balance-rrhh.tsx
@@ -1,10 +1,12 @@
+import React, { useState } from 'react';
 import SearchInput from '@/components/atoms/searchInput';
 import CardDeparment from '@/components/molecules/cardDeparment';
-import React from 'react';
 
 export default function ScoreBalanceRrhh() {
-
-  // Cards deparment
+  // Estado para almacenar el valor de búsqueda
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  // Data Cards deparment
   const departments = [
     {
       department: 'Dirección de Riesgos',
@@ -19,6 +21,26 @@ export default function ScoreBalanceRrhh() {
     },
     {
       department: 'Finanzas',
+      direction: 'Recursos Humanos',
+      imageUrls: [
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      ],
+      progressValue: 20,
+    },
+
+    {
+      department: 'Direccion general',
+      direction: 'Recursos Humanos',
+      imageUrls: [
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      ],
+      progressValue: 10,
+    },
+
+    {
+      department: 'Direccion Operacionales',
       direction: 'Administración',
       imageUrls: [
         'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
@@ -29,22 +51,36 @@ export default function ScoreBalanceRrhh() {
     // Agrega más departamentos aquí
   ];
 
+  // Filtra los departamentos según el término de búsqueda
+  const filteredDepartments = departments.filter(dept =>
+    dept.department.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="p-10">
-      <p className="relative top-1 font-normal text-xl mb-3">Lista de departamentos</p>
-      <SearchInput placeholder={'Buscar departamento'} />
+      <p className=" text-center relative top-1 font-normal text-xl mb-3">Lista de departamentos</p>
+
+      {/* SearchInput onChange actualiza el valor de búsqueda */}
+      <SearchInput
+        placeholder={'Buscar departamento'}
+        onChange={e => setSearchTerm(e.target.value)}
+      />
 
       <div className="mt-5">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6 gap-4">
-          {departments.map((dept, index) => (
-            <CardDeparment
-              key={index}
-              department={dept.department}
-              direction={dept.direction}
-              imageUrls={dept.imageUrls}
-              progressValue={dept.progressValue}
-            />
-          ))}
+          {filteredDepartments.length > 0 ? (
+            filteredDepartments.map((dept, index) => (
+              <CardDeparment
+                key={index}
+                department={dept.department}
+                direction={dept.direction}
+                imageUrls={dept.imageUrls}
+                progressValue={dept.progressValue}
+              />
+            ))
+          ) : (
+            <p>No se encontraron departamentos</p>
+          )}
         </div>
       </div>
     </div>
