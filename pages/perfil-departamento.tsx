41f5e2d16de2e324b6976efa4c6d6b7deba4db27
frontmappriskdr <front@mappriskdr.com>
2024-10-02 10:03:32 -0400

    file profile deparment add

--- pages/perfil-departamento.tsx
+++ pages/perfil-departamento.tsx
@@ -2,8 +2,26 @@ import React from 'react'
 
 export default function perfilDepartamento() {
   return (
-    <div>
-        perfil-departamento
+    <div className='p-5'>
+        <div className="grid grid-cols-8 grid-rows-5 gap-4">
+
+            {/* Info person */}
+            <div className="col-span-2 row-span-5">
+            <span className="relative inline-block">
+            <img
+            alt=""
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            className="h-28 w-28 rounded-full"
+            />
+             <span className="absolute left-[5.6rem] top-3 block h-9 w-9 rounded-full bg-gray-300 ring-4 ring-white" /></span>
+
+            </div>
+
+            {/* graphic */}
+            <div className="col-span-6 row-span-5 col-start-3">
+                11
+            </div>
+        </div>
     </div>
   )
 }
--- pages/rrhh/score-balance-rrhh.tsx
+++ pages/rrhh/score-balance-rrhh.tsx
@@ -17,7 +17,19 @@ export default function ScoreBalanceRrhh() {
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       ],
-      progressValue: 20,
+      progressValue:50,
+      src: '/perfil-departamento', // Enlace de ejemplo
+    },
+    {
+      department: 'Dirección de Riesgos',
+      direction: 'Recursos Humanos',
+      imageUrls: [
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      ],
+      progressValue: 100,
       src: '/perfil-departamento', // Enlace de ejemplo
     },
     {
