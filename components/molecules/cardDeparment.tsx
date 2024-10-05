cb550ee8a98171ed77d5b4f8b2dfc07ed0e8f5cb
frontmappriskdr <front@mappriskdr.com>
2024-10-05 01:59:50 -0400

    edit color text in the card deparment

--- components/molecules/cardDeparment.tsx
+++ components/molecules/cardDeparment.tsx
@@ -23,14 +23,14 @@ const CardDeparment: React.FC<CardDeparmentProps> = ({
       <div className="text-[0.9rem] font-light rounded-lg w-full md:w-full lg:w-60 p-4 border-solid border-[1px] border-[#E0E2E5] hover:bg-gray-100 cursor-pointer">
         {/* Departamento */}
         <div className="mb-2">
-          <p className="text-gray-500 text-[0.8rem] font-semibold">Departamento</p>
-          <p>{department}</p>
+          <p className="text-[0.8rem] leading-6 font-semibold">Departamento</p>
+          <p className='text-gray-700'>{department}</p>
         </div>
         
         {/* Dirección */}
         <div className="mb-6">
-          <p className="text-gray-500 text-[0.8rem] font-semibold">Dirección</p>
-          <p>{direction}</p>
+          <p className="text-[0.8rem] leading-6 font-semibold">Dirección</p>
+          <p className='text-gray-700'>{direction}</p>
         </div>
 
         {/* Imagenes */}
