1c2e19f4a5996bf03dcd9580aeeb8bb61ac9296b
frontmappriskdr <front@mappriskdr.com>
2024-10-05 14:15:44 -0400

    modal list objets

--- components/molecules/listUiV2.tsx
+++ components/molecules/listUiV2.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProgresBar from '../atoms/progresBar';
 import { EyeIcon } from '@heroicons/react/20/solid';
+import ModalUi from '../organisms/modalUi';
 
 interface ListUiV2Props {
   objetivoTitle: string;
@@ -17,6 +18,14 @@ export default function ListUiV2({
   objetivoEspecifico,
   progressValue,
 }: ListUiV2Props) {
+  // Estado para manejar la apertura/cierre del modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // Función para abrir y cerrar el modal
+  const handleModalToggle = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className="text-[13px] my-8">
       <p className="mb-4">{objetivoTitle}</p>
@@ -45,7 +54,10 @@ export default function ListUiV2({
         {/* Icono Ver más */}
         <div className="md:col-span-8 lg:col-span-2 col-span-8">
           <div className="float-right">
-            <div className="w-12 h-12 flex items-center justify-center rounded-full hover:cursor-pointer hover:bg-gray-100 transition-colors duration-300">
+            <div
+              className="w-12 h-12 flex items-center justify-center rounded-full hover:cursor-pointer hover:bg-gray-100 transition-colors duration-300"
+              onClick={handleModalToggle} // Agrega el evento de clic para abrir el modal
+            >
               <EyeIcon className="w-6 h-6 text-gray-600" />
             </div>
           </div>
@@ -57,6 +69,33 @@ export default function ListUiV2({
         <ProgresBar progressValue={progressValue} />
       </p>
       <hr />
+
+      {/* Modal que se abrirá al hacer clic en el icono */}
+      <ModalUi isOpen={isModalOpen} onClose={handleModalToggle}>
+        {/* Contenido del Modal */}
+        <div className='text-[0.9rem] font-light'>
+            <p className="text-lg mb-5 font-semibold">Detalles del objetivo</p>
+
+            <div className="mt-3">
+              <p className='font-normal'>Meta operativa:</p>
+              <p> {metaOperativa}</p>
+            </div>
+            <div>
+            <div className="mt-10">
+              <p className='font-normal'>Fecha de cierre:</p>
+              <p>{fechaCierre}</p>
+            </div>
+            </div>
+        
+            <div>
+             <div className="mt-10">
+              <p className='font-normal'>Objetivo específico:</p>
+              <p>{objetivoEspecifico}</p>
+             </div>
+
+            </div>
+        </div>
+      </ModalUi>
     </div>
   );
 }
