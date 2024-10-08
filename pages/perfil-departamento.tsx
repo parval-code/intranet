a01cc67179363b2339eeb0e58c4d0d2db76a84fe
frontmappriskdr <front@mappriskdr.com>
2024-10-08 19:39:14 -0400

    update branch

--- pages/perfil-departamento.tsx
+++ pages/perfil-departamento.tsx
@@ -168,6 +168,7 @@ export default function perfilDepartamento() {
                       className="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-parvalColor sm:text-sm sm:leading-6"
                     >
                       <option>Calificacion Peers</option>
+                      <option>1</option>
                       <option>2</option>
                       <option>3</option>
                     </select>
